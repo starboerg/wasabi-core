316ecde9110ed5b9aee79172f5a5ce7214f4ccc0
Frank Förster <frank.foerster@sandstein.de>
2015-04-08 22:01:42 +0200

    add longterm cache config

--- config/cache.php
+++ config/cache.php
@@ -1,6 +1,12 @@
 <?php
 return [
     'Cache' => [
+        'wasabi/core/longterm' => [
+            'className' => 'File',
+            'duration' => '+999 days',
+            'prefix' => '',
+            'path' => CACHE . 'wasabi' . DS . 'core' . DS . 'longterm'
+        ],
         'wasabi/core/guardian_paths' => [
             'className' => 'File',
             'duration' => '+999 days',
