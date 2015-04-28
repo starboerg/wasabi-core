85d49b4173614f807deeb8486118cd1be222b9a1
Frank Förster <frank.foerster@sandstein.de>
2015-04-28 19:16:39 +0200

    implement language edit action

--- src/Controller/LanguagesController.php
+++ src/Controller/LanguagesController.php
@@ -11,6 +11,8 @@
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace Wasabi\Core\Controller;
+use Cake\Network\Exception\NotFoundException;
+use Cake\Network\Exception\MethodNotAllowedException;
 
 /**
  * Class LanguagesController
@@ -58,5 +60,36 @@ class LanguagesController extends BackendAppController
             }
         }
         $this->set('language', $language);
+    }
+
+    /**
+     * Edit action
+     * GET | PUT
+     *
+     * @param string $id
+     */
+    public function edit($id)
+    {
+        if (!$id || !$this->Languages->exists($id)) {
+            throw new NotFoundException();
+        }
+        if (!$this->request->is(['get', 'put'])) {
+            throw new MethodNotAllowedException();
+        }
+
+        $language = $this->Languages->get($id);
+        if ($this->request->is('put')) {
+            $this->Languages->patchEntity($language, $this->request->data);
+            if ($this->Languages->save($language)) {
+                $this->Flash->success(__d('wasabi_core', 'The language <strong>{0}</strong> has been saved.', $language->name));
+            } else {
+                $this->Flash->error($this->dbErrorMessage);
+            }
+            $this->redirect(['action' => 'index']);
+            return;
+        }
+
+        $this->set('language', $language);
+        $this->render('add');
     }
 }
