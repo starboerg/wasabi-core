8fc1ed390973a0c42021a8e5dccd3178f0c009c5
frankfoerster <github@frankfoerster.com>
2015-05-10 13:54:08 +0200

    implement SettingsController::cache() with tpl, entity and table

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -11,12 +11,15 @@
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace Wasabi\Core\Controller;
+use Wasabi\Core\Model\Entity\CacheSetting;
 use Wasabi\Core\Model\Entity\GeneralSetting;
+use Wasabi\Core\Model\Table\CacheSettingsTable;
 use Wasabi\Core\Model\Table\GeneralSettingsTable;
 
 /**
  * Class SettingsController
  *
+ * @property CacheSettingsTable $CacheSettings
  * @property GeneralSettingsTable $GeneralSettings
  */
 class SettingsController extends BackendAppController
@@ -29,12 +32,18 @@ class SettingsController extends BackendAppController
      */
     public function general()
     {
+        $keys = [
+            'instance_name',
+            'Login__Message__show',
+            'Login__Message__text',
+            'Login__Message__class'
+        ];
         $this->GeneralSettings = $this->loadModel('Wasabi/Core.GeneralSettings');
-        $settings = $this->GeneralSettings->getKeyValues(new GeneralSetting());
+        $settings = $this->GeneralSettings->getKeyValues(new GeneralSetting(), $keys);
         if ($this->request->is('post') && !empty($this->request->data)) {
             $settings = $this->GeneralSettings->newEntity($this->request->data);
             if (!$settings->errors()) {
-                if ($this->GeneralSettings->saveKeyValues($settings)) {
+                if ($this->GeneralSettings->saveKeyValues($settings, $keys)) {
                     $this->Flash->success(__d('wasabi_core', 'The general settings have been updated.'));
                     $this->redirect(['action' => 'general']);
                     return;
@@ -45,8 +54,34 @@ class SettingsController extends BackendAppController
                 $this->Flash->error($this->formErrorMessage);
             }
         }
+        $this->set('settings', $settings);
+    }
+
+    public function cache()
+    {
+        $keys = [
+            'enable_caching',
+            'cache_duration'
+        ];
+        $this->CacheSettings = $this->loadModel('Wasabi/Core.CacheSettings');
+        $settings = $this->CacheSettings->getKeyValues(new CacheSetting(), $keys);
+        if ($this->request->is('post') && !empty($this->request->data)) {
+            $settings = $this->CacheSettings->newEntity($this->request->data);
+            if (!$settings->errors()) {
+                if ($this->CacheSettings->saveKeyValues($settings, $keys)) {
+                    $this->Flash->success(__d('wasabi_core', 'The cache settings have been updated.'));
+                    $this->redirect(['action' => 'cache']);
+                    return;
+                } else {
+                    $this->Flash->error($this->dbErrorMessage);
+                }
+            } else {
+                $this->Flash->error($this->formErrorMessage);
+            }
+        }
         $this->set([
-            'settings' => $settings
+            'settings' => $settings,
+            'cacheDurations' => $this->CacheSettings->cacheDurations
         ]);
     }
 }
---
+++ src/Model/Table/CacheSettingsTable.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Wasabi CMS
+ * Copyright (c) Frank Förster (http://frankfoerster.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Frank Förster (http://frankfoerster.com)
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Wasabi\Core\Model\Table;
+use Cake\ORM\Entity;
+use Cake\Validation\Validator;
+use ArrayObject;
+use Cake\Event\Event;
+use Wasabi\Core\Model\Entity\Setting;
+
+/**
+ * Class CacheSettingsTable
+ *
+ * @method getKeyValues(Entity $entity, array $keys) KeyValueBehavior::getKeyValues(Entity $entity, array $keys)
+ * @method saveKeyValues(Entity $entity, array $keys) KeyValueBehavrio::saveKeyValues(Entity $entity, array $keys)
+ */
+class CacheSettingsTable extends SettingsTable
+{
+    /**
+     * Holds all select options for cache_duration.
+     *
+     * @var array
+     */
+    public $cacheDurations = [];
+
+    /**
+     * Initialize a table instance. Called after the constructor.
+     *
+     * @param array $config Configuration options passed to the constructor
+     */
+    public function initialize(array $config)
+    {
+        $this->table('settings');
+
+        $this->addBehavior('Wasabi/Core.KeyValue', [
+            'scope' => 'Core'
+        ]);
+
+        $this->cacheDurations = array(
+            '1 hour' => __d('wasabi_core', '1 hour'),
+            '2 hours' => __d('wasabi_core', '{0} hours', 2),
+            '4 hours' => __d('wasabi_core', '{0} hours', 4),
+            '8 hours' => __d('wasabi_core', '{0} hours', 8),
+            '16 hours' => __d('wasabi_core', '{0} hours', 16),
+            '1 day' => __d('wasabi_core', '1 day'),
+            '2 days' => __d('wasabi_core', '{0} days', 2),
+            '5 days' => __d('wasabi_core', '{0} days', 5),
+            '7 days' => __d('wasabi_core', '{0} days', 7),
+            '14 days' => __d('wasabi_core', '{0} days', 14),
+            '30 days' => __d('wasabi_core', '{0} days', 30),
+            '60 days' => __d('wasabi_core', '{0} days', 60),
+            '90 days' => __d('wasabi_core', '{0} days', 90),
+            '180 days' => __d('wasabi_core', '{0} days', 180),
+            '365 days' => __d('wasabi_core', '{0} days', 365),
+            '999 days' => __d('wasabi_core', '{0} days', 999)
+        );
+
+        parent::initialize($config);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param Validator $validator
+     * @return Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        return $validator
+            ->requirePresence('enable_caching', true, __d('wasabi_core', 'Please select a cache status.'))
+            ->add('enable_caching', 'inList', [
+                'rule' =>  ['inList', ['0', '1']],
+                'message' => __d('wasabi_core', 'Please select a valid cache status.')
+            ])
+            ->requirePresence('cache_duration', true, __d('wasabi_core', 'Please select a cache duration.'))
+            ->add('cache_duration', 'custom', [
+                'rule' => function ($value, $context) {
+                    return in_array($value, $this->cacheDurations);
+                },
+                'message' => __d('wasabi_core', 'Please select a valid cache duration.')
+            ]);
+    }
+}
