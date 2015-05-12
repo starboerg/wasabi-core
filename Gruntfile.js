5eb75541e91d1d6f8cfc7b79465a93fbdba7b45c
frankfoerster <github@frankfoerster.com>
2015-05-12 23:46:16 +0200

    remove grun-contrib-imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -187,7 +187,6 @@ module.exports = function(grunt) {
   ]);
 
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
