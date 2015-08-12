959439e0ac29c27860aae523e29df970e1e71bf1
frankfoerster <github@frankfoerster.com>
2015-08-12 22:52:19 +0200

    add event for Esc key to close the dialog, add left and right sidebar support

--- src/Assets/js/common/DialogView.js
+++ src/Assets/js/common/DialogView.js
@@ -1,12 +1,28 @@
 define(function(require) {
 
   var $ = require('jquery');
+  var _ = require('underscore');
   var Handlebars = require('handlebars');
   var BaseView = require('common/BaseView');
 
   return BaseView.extend({
 
     templateData: {},
+
+    /**
+     * Holds a reference to the left sidebar.
+     */
+    $sidebarLeft: {},
+
+    /**
+     * Holds a reference to the right sidebar.
+     */
+    $sidebarRight: {},
+
+    /**
+     * Holds a reference to the content container.
+     */
+    $content: {},
 
     events: {
       'click [data-toggle="close"]': 'closeDialog'
@@ -22,11 +38,28 @@ define(function(require) {
       }
       this.setElement(this.template(this.templateData));
       $('body').addClass('dialog-open').append(this.$el);
+
+      $(window).on('keyup', _.bind(this.onKeyup, this));
+
+      this.$sidebarLeft = this.$('dialog-sidebar-left');
+      this.$sidebarRight = this.$('dialog-sidebar-right');
+      this.$content = this.$('.dialog-content');
+
+      if (typeof this.initDialogContent === 'function') {
+        this.initDialogContent();
+      }
     },
 
     closeDialog: function() {
+      $(window).off('keyup', _.bind(this.onKeyup, this));
       this.destroy();
       $('body').removeClass('dialog-open');
+    },
+
+    onKeyup: function(event) {
+      if (event.which === 27) {
+        this.closeDialog();
+      }
     }
 
   });
