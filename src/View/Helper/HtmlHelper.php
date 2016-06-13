5136d1c6139eed502b0baef1eea43c0d86c08928
Frank Förster <frank.foerster@sandstein.de>
2016-06-13 14:54:57 +0200

    add a new unprotected confirmation link method that can be handled via js

--- src/View/Helper/HtmlHelper.php
+++ src/View/Helper/HtmlHelper.php
@@ -13,6 +13,8 @@
 namespace Wasabi\Core\View\Helper;
 
 use Cake\Core\Configure;
+use Cake\Routing\Router;
+use Cake\Utility\Hash;
 
 /**
  * Class HtmlHelper
@@ -99,6 +101,47 @@ class HtmlHelper extends \Cake\View\Helper\HtmlHelper
     }
 
     /**
+     * Create a confirmation link.
+     *
+     * @param string $title The link title.
+     * @param array $options Options passed to the html link helper method.
+     * @return string
+     */
+    public function confirmationLink($title, $options)
+    {
+        if (!isset($options['confirm-message'])) {
+            user_error('\'confirm-message\' option is not set on protectedConfirmationLink.');
+            $options['confirm-message'] = '';
+        }
+        if (!isset($options['confirm-title'])) {
+            user_error('\'confirm-title\' option is not set on protectedConfirmationLink.');
+            $options['confirm-title'] = '';
+        }
+
+        $url = 'javascript:void(0)';
+
+        $linkOptions = [
+            'data-modal-header' => $options['confirm-title'],
+            'data-modal-body' => '<p>' . $options['confirm-message'] . '</p>',
+            'data-toggle' => 'confirm'
+        ];
+        unset($options['confirm-title'], $options['confirm-message']);
+
+        if (isset($options['notify'])) {
+            $linkOptions['data-modal-notify'] = $options['notify'];
+            unset($options['notify']);
+        }
+
+        if (isset($options['event'])) {
+            $linkOptions['data-modal-event'] = $options['event'];
+            unset($options['event']);
+        }
+
+        $linkOptions = Hash::merge($linkOptions, $options);
+        return $this->link($title, $url, $linkOptions);
+    }
+
+    /**
      * Used internally to render the title (primary and secondary) of the title pad.
      *
      * @param string $title The title.
@@ -114,5 +157,24 @@ class HtmlHelper extends \Cake\View\Helper\HtmlHelper
         $out .= '</h1>';
 
         return $out;
+    }
+
+    /**
+     * Transform the supplied $url into a properly prefixed backend url.
+     *
+     * @param array|string $url The url to transform.
+     * @param bool $rel Whether to generate a relative or absolute url.
+     * @return array|string
+     */
+    protected function _getUrl($url, $rel = false)
+    {
+        if (!is_array($url)) {
+            $url = ltrim($url, '/');
+            $url = '/' . $url;
+        }
+        if ($rel !== false) {
+            $url = Router::url($url);
+        }
+        return $url;
     }
 }
