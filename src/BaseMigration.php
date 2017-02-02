1136ef16d3b4f43d7a43ce7047daaf0200b805f2
frankfoerster <github@frankfoerster.com>
2017-02-02 17:07:10 +0100

    add BaseMigration class that is now used by all wasabi core migrations

---
+++ src/BaseMigration.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Wasabi\Core;
+
+use Cake\Cache\Cache;
+use Migrations\AbstractMigration;
+use Phinx\Db\Table;
+use Phinx\Db\Table\Column;
+
+abstract class BaseMigration extends AbstractMigration
+{
+    /**
+     * Initialize the migration.
+     *
+     * @return void
+     */
+    public function init()
+    {
+        parent::init();
+
+        Cache::clear();
+    }
+
+    /**
+     * Make the id primary column an unsigned integer.
+     *
+     * @param Table $table
+     * @return void
+     */
+    public function unsignedIntId(Table $table)
+    {
+        $id = new Column();
+        $id->setIdentity(true)
+            ->setType('integer')
+            ->setOptions(['limit' => 11, 'signed' => false, 'null' => false]);
+
+        $table->changeColumn('id', $id);
+    }
+
+    /**
+     * Clear the cake model cache.
+     */
+    public function clearModelCache()
+    {
+        Cache::clear(false, '_cake_model_');
+    }
+}
