5e5d77571b0dded1c9b34db3e0b622074eabf417
frankfoerster <github@frankfoerster.com>
2017-02-02 16:53:10 +0100

    update Wasabi/Core config (removed settings are now handled via backend interface)

--- config/config.php
+++ config/config.php
@@ -1,17 +1,24 @@
 <?php
+/**
+ * Wasabi Core
+ * Copyright (c) Frank Förster (http://frankfoerster.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Frank Förster (http://frankfoerster.com)
+ * @link          https://github.com/wasabi-cms/core Wasabi Project
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
 return [
     'Wasabi' => [
         'Auth' => [
-            'loginWithUsernamePassword' => true,
-            'loginWithEmailPassword' => false,
-            'simplePermissions' => true
-        ],
-        'User' => [
-            'hasUsername' => true,
-            'hasSalutation' => false,
-            'hasFirstnameLastname' => false,
-            'belongsToManyGroups' => false,
-            'allowTimezoneChange' => true
+            'identity_field' => 'email',
+            'identity_label' => __d('wasabi_core', 'Email'),
+            'password_field' => 'password',
+            'password_label' => __d('wasabi_core', 'Password')
         ]
     ]
 ];
