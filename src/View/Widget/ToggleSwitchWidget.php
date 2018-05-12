7178ec9e096a38ce097ea586bcbe368f9f9a0201
frankfoerster <github@frankfoerster.com>
2018-05-13 01:43:22 +0200

    add missing header comments

--- src/View/Widget/ToggleSwitchWidget.php
+++ src/View/Widget/ToggleSwitchWidget.php
@@ -1,5 +1,16 @@
 <?php
-
+/**
+ * Wasabi Core
+ * Copyright (c) Frank Förster (http://frankfoerster.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Frank Förster (http://frankfoerster.com)
+ * @link          https://github.com/wasabi-cms/core Wasabi Project
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 namespace Wasabi\Core\View\Widget;
 
 use Cake\View\Form\ContextInterface;
