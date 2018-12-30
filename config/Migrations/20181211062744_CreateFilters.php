a6fd266917589e1f3076fce9eb723128a883162b
Patrick Brösamle <patrick.broesamle@gmx.de>
2018-12-30 12:36:39 +0100

    added missing migration of filters table (#4)

---
+++ config/Migrations/20181211062744_CreateFilters.php
@@ -0,0 +1,43 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateFilters extends AbstractMigration
+{
+    /**
+     * Migrate up.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $table = $this->table('filters');
+        $table->addColumn('plugin', 'string', ['limit' => 255, 'null' => true, 'default' => null])
+            ->addColumn('controller', 'string', ['limit' => 255, 'null' => false])
+            ->addColumn('action', 'string', ['limit' => 255, 'null' => false])
+            ->addColumn('slug', 'string', ['limit' => 14, 'null' => false])
+            ->addColumn('filter_data', 'text', ['null' => false])
+            ->addColumn('created', 'datetime', ['null' => false, 'default' => 'CURRENT_TIMESTAMP']);
+        $table->addIndex('slug', ['name' => 'BY_SLUG', 'unique' => true]);
+        $table->create();
+
+        $id = new Column();
+        $id->setIdentity(true)
+            ->setType('integer')
+            ->setOptions(['limit' => 11, 'signed' => false, 'null' => false]);
+
+        $table->changeColumn('id', $id)->save();
+
+        Cache::clear();
+    }
+
+    /**
+     * Migrate down
+     *
+     * @return void
+     */
+    public function down() {
+        $this->table('filters')->drop();
+
+        Cache::clear();
+    }
+}
--- src/Model/Entity/Filter.php
+++ src/Model/Entity/Filter.php
@@ -4,6 +4,17 @@ namespace Wasabi\Core\Model\Entity;
 
 use Cake\ORM\Entity;
 
+/**
+ * Class Filter
+ *
+ * @property int $id
+ * @property string $plugin
+ * @property string $controller
+ * @property string $action
+ * @property string $slug
+ * @property string $filter_data
+ * @property \DateTime $created
+ */
 class Filter extends Entity
 {
 
